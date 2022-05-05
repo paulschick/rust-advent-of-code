40a45b74fd62fb81f9ddc908c4d6c5313636f189
paulschick <paulschick19@gmail.com>
2022-05-05 18:48:47 -0400

    Adds iterator example through vec

--- 1-sonar-sweep/src/main.rs
+++ 1-sonar-sweep/src/main.rs
@@ -15,6 +15,17 @@ use std::io::{Read, Result};
 ///         I need to read the input from the input.txt file. That's going to be my first challenge
 ///          personally...
 ///
+/// Step 2:
+///         Read the number of times that the number increases from the previous number.
+///         Okay so this is more challenging (when you don't know rust) than it sounds like.
+///
+/// Current count -> mutable integer
+/// Previous number -> mutable integer
+/// Could use recursion through the vector. Have a function (parent function) hold the mutable
+/// variables. This then calls another function recursively. That function will return a boolean.
+///     True -> number increased
+///     False -> number did not increase
+///
 
 fn read_input_file() -> Result<Vec<u8>> {
     let mut file = File::open("input.txt")?;
@@ -23,7 +34,17 @@ fn read_input_file() -> Result<Vec<u8>> {
     return Ok(data);
 }
 
+fn count_increases(vec: &Vec<u8>) {
+    for i in 0..vec.len() {
+        let current_value = vec[i];
+        if i != 0 {
+            let previous_value = vec[i - 0];
+            println!("Current Value: {}, Previous Value: {}", current_value, previous_value);
+        }
+    }
+}
+
 fn main() {
     let data = read_input_file();
-    println!("{:?}", data);
+    count_increases(&data.unwrap());
 }
