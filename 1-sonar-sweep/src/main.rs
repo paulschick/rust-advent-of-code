9176a1dc56fd5d9b3e89392fcaa25a7267611871
paulschick <paulschick19@gmail.com>
2022-05-05 18:34:34 -0400

    Adds text input for challenge 1

--- 1-sonar-sweep/src/main.rs
+++ 1-sonar-sweep/src/main.rs
@@ -1,3 +1,16 @@
+/// https://adventofcode.com/2021/day/1
+///
+/// Antenna strength: 0 - 50
+/// Must get 50 starts by 12/25 (Some Christmas stuff)
+/// - Collect by solving puzzles
+/// - Two are available each day in the Advent calendar
+/// - Second puzzle unlocked after completing the first
+/// - One challenge = one star
+///
+/// Step 1:
+///         I need to read the input from the input.txt file. That's going to be my first challenge
+///          personally...
+///
 fn main() {
     println!("Hello, world!");
 }
