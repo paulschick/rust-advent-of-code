fbfb16a2f6966112ecfaafa45ad57e294cc18d38
paulschick <paulschick19@gmail.com>
2022-05-06 08:11:07 -0400

    Adds project two with input file

---
+++ 2-dive/src/main.rs
@@ -0,0 +1,15 @@
+/// https://adventofcode.com/2021/day/2
+/// use input.txt
+///
+///two directions -> horizontal, depth
+///direction + number adds/removes that amount from position
+///forward -> add to horizontal
+///down -> add to depth
+///up -> decrease depth
+///
+///That's all. No backwards or anything
+///
+///
+fn main() {
+    println!("Hello, world!");
+}
