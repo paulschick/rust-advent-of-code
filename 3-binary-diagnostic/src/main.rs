275d7cdf5785275c19d7fda003b16567f976d0b7
paulschick <paulschick19@gmail.com>
2022-05-06 18:08:09 -0400

    3 - configure logging to standard out

--- 3-binary-diagnostic/src/main.rs
+++ 3-binary-diagnostic/src/main.rs
@@ -1,6 +1,20 @@
+use std::env;
+use env_logger::{Builder, Target};
+
+#[macro_use]
+extern crate log;
+
+fn init_logger() {
+    // log without calling from command line
+    env::set_var("RUST_LOG", "info");
+    let mut builder = Builder::from_default_env();
+    builder.target(Target::Stdout);
+    builder.init();
+}
 
 fn main() {
-    env_logger::init();
-    println!("hello");
-    log::warn!("[warn] hello");
+    init_logger();
+    info!("Starting");
+    info!("hello");
+    warn!("warning you");
 }
