8cea3d59c3c58b2279e1d4d1f2f3d92a274309cb
paulschick <paulschick19@gmail.com>
2022-05-06 18:21:43 -0400

    3 - adds append to file function

--- 3-binary-diagnostic/src/main.rs
+++ 3-binary-diagnostic/src/main.rs
@@ -1,4 +1,6 @@
 use std::env;
+use std::fs::{File, OpenOptions};
+use std::io::{Write, Error};
 use env_logger::{Builder, Target};
 
 #[macro_use]
@@ -12,9 +14,29 @@ fn init_logger() {
     builder.init();
 }
 
+fn write_log() -> Result<(), Error> {
+    let path = "test-log.log";
+    let mut output = File::create(path)?;
+    write!(output, "Hello from this program\n")?;
+
+    return Ok(());
+}
+
+fn append_file() {
+    let mut file = OpenOptions::new()
+        .append(true)
+        .open("test-log.log")
+        .expect("unable to open file");
+    file.write_all("Hello from a new line\n".as_bytes())
+        .expect("write failed");
+    println!("Success");
+}
+
 fn main() {
     init_logger();
     info!("Starting");
     info!("hello");
     warn!("warning you");
+    write_log().unwrap();
+    append_file();
 }
