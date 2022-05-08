31e597ca8e72282c32597f7e1efc2fd01d28de14
paulschick <paulschick19@gmail.com>
2022-05-08 11:47:01 -0400

    Create board from lines, adds filter to row parse function

--- 4.1-giant-squid/src/main.rs
+++ 4.1-giant-squid/src/main.rs
@@ -26,6 +26,7 @@ fn parse_selected_answers(row: &str) -> Vec<u32> {
 /// Creates an array of fixed length given a row as a string slice
 fn create_row_array(row: &str) -> [u32; 5] {
     row.split(" ")
+        .filter(|v| v != &"")
         .map(|v| v.trim()
             .to_string()
             .parse::<u32>()
@@ -36,10 +37,42 @@ fn create_row_array(row: &str) -> [u32; 5] {
         .unwrap()
 }
 
+/// Must pass in a Vec<&str> that contains only the lines for a given board.
+/// The length of the Vec should be 5, and the length of each &str should be five.
+/// This function is not responsible for determining the indices of the master Vec.
+fn parse_board_lines(lines: Vec<&str>) -> [[u32; 5]; 5] {
+    let mut board: [[u32; 5]; 5] = [[0u32; 5]; 5];
+    for i in 0..lines.len() {
+        board[i] = create_row_array(lines[i]);
+    }
+    return board;
+}
+
 
 fn main() {
-    let test_row_data: &str = 
-        "31 88 71 23 61";
-    let array = create_row_array(test_row_data);
-    println!("{:?}", array);
+    // let test_row_data: &str = 
+    //     "31 88 71 23 61";
+    // let array = create_row_array(test_row_data);
+    // println!("{:?}", array);
+
+    let lines = parse_file_lines();
+    let selected_answers_row = &lines[0];
+    let selected_answers_vec = parse_selected_answers(&selected_answers_row);
+    println!("Selected Answers {:?}", selected_answers_vec);
+
+    // board start, board end indices
+    let test_start = usize::try_from(2).unwrap();
+    let test_end = usize::try_from(6).unwrap();
+
+    let mut test_board_lines: Vec<&str> = vec![];
+
+    for i in test_start..=test_end {
+        test_board_lines.push(&lines[i].trim());
+    }
+
+    println!("Test Board Strings {:?}", test_board_lines);
+
+    let test_board = parse_board_lines(test_board_lines);
+
+    println!("{:?}", test_board);
 }
