cd69051e29ffdd0f20775e557cb432c7c8f65efa
paulschick <paulschick19@gmail.com>
2022-05-08 11:34:58 -0400

    Creates function to parse selected answers from row

--- 4.1-giant-squid/src/main.rs
+++ 4.1-giant-squid/src/main.rs
@@ -1,3 +1,28 @@
+#![allow(dead_code)]
+
+use std::{fs::File, io::{prelude::*, BufReader}};
+
+/// Read in the input file and return lines
+fn parse_file_lines() -> Vec<String> {
+    BufReader::new(
+        File::open("input.txt")
+            .expect("File not found!")
+        )
+        .lines()
+        .map(|l| l.expect("unable to parse line!"))
+        .collect()
+}
+
+fn parse_selected_answers(row: &str) -> Vec<u32> {
+    row.split(",")
+        .map(|v| v.trim()
+            .to_string()
+            .parse::<u32>()
+            .unwrap()
+            )
+        .collect::<Vec<u32>>()
+}
+
 /// Creates an array of fixed length given a row as a string slice
 fn create_row_array(row: &str) -> [u32; 5] {
     row.split(" ")
