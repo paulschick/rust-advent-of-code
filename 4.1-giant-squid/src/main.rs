731d1981876ca94b6941b905b9c55850d66ea5d1
paulschick <paulschick19@gmail.com>
2022-05-13 16:51:53 -0400

    Adds profile examples to giant squid cargo.toml

--- 4.1-giant-squid/src/main.rs
+++ 4.1-giant-squid/src/main.rs
@@ -1,13 +1,13 @@
 #![allow(dead_code)]
 
-use std::{fs::File, io::{prelude::*, BufReader}};
+use std::{
+    fs::File,
+    io::{prelude::*, BufReader},
+};
 
 /// Read in the input file and return lines
 fn parse_file_lines() -> Vec<String> {
-    BufReader::new(
-        File::open("input.txt")
-            .expect("File not found!")
-        )
+    BufReader::new(File::open("input.txt").expect("File not found!"))
         .lines()
         .map(|l| l.expect("unable to parse line!"))
         .collect()
@@ -15,11 +15,7 @@ fn parse_file_lines() -> Vec<String> {
 
 fn parse_selected_answers(row: &str) -> Vec<u32> {
     row.split(",")
-        .map(|v| v.trim()
-            .to_string()
-            .parse::<u32>()
-            .unwrap()
-            )
+        .map(|v| v.trim().to_string().parse::<u32>().unwrap())
         .collect::<Vec<u32>>()
 }
 
@@ -27,11 +23,7 @@ fn parse_selected_answers(row: &str) -> Vec<u32> {
 fn create_row_array(row: &str) -> [u32; 5] {
     row.split(" ")
         .filter(|v| v != &"")
-        .map(|v| v.trim()
-            .to_string()
-            .parse::<u32>()
-            .unwrap()
-            )
+        .map(|v| v.trim().to_string().parse::<u32>().unwrap())
         .collect::<Vec<u32>>()
         .try_into()
         .unwrap()
@@ -48,9 +40,11 @@ fn parse_board_lines(lines: Vec<&str>) -> [[u32; 5]; 5] {
     return board;
 }
 
-
+/// Each board is [[u32; 5]; 5]
+/// So I need all boards to be Vec<[[u32; 5]; 5]>
+/// I still don't know how to track this.
 fn main() {
-    // let test_row_data: &str = 
+    // let test_row_data: &str =
     //     "31 88 71 23 61";
     // let array = create_row_array(test_row_data);
     // println!("{:?}", array);
